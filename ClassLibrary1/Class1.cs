89bd5097fc8549154bbba7265283652025bc2609
User-XXI <andmak96@gmail.com>
2022-12-12 14:04:09 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Animals
+{
+    public enum eClassificationAnimal
+    {
+        Herbivores = 1,
+        Carnivores = 2,
+        Omnivores = 3
+    }
+
+    public enum eFavouriteFood
+    {
+        Meat = 1,
+        Plants = 2,
+        Everything = 3
+    }
+
+    public abstract class Animal
+    {
+        public string Country { get; set; } = "None";
+        public bool HideFromOtherAnimals { get; set; } = false;
+        public string Name { get; set; } = "None";
+        public string WhatAnimal { get; set; }
+
+        public Animal() { }
+
+        public Animal( string _country, bool _hide, string _name )
+        {
+            Country = _country;
+            HideFromOtherAnimals = _hide;
+            Name = _name;
+        }
+
+        public void Deconstruct() { }
+
+        public abstract Enum GetClassificationAnimal();
+
+        public abstract Enum GetFavouriteFood();
+
+        public abstract void SayHello();
+
+        public void Print()
+        {
+            Console.WriteLine( "Name: " + Name );
+            Console.WriteLine( "Classification: " + GetClassificationAnimal().ToString() );
+            Console.WriteLine( "Favourite food: " + GetFavouriteFood().ToString() );
+            Console.WriteLine( "Country: " + Country );
+            Console.WriteLine( "Hide from other animals: " + HideFromOtherAnimals );
+            Console.WriteLine( "What is this animal: " + WhatAnimal );
+
+        }
+    }
+
+    public class Cow : Animal
+    {
+        public Cow() : base() { this.WhatAnimal = "Cow"; }
+
+        public Cow( string _country, bool _hide, string _name ) : base( _country, _hide, _name ) { this.WhatAnimal = "Cow"; }
+
+        public override Enum GetClassificationAnimal()
+        {
+            return (eClassificationAnimal)1;
+        }
+
+        public override Enum GetFavouriteFood()
+        {
+            return (eFavouriteFood)2;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine( "Hello! I am " + WhatAnimal + " " + Name + " from " + Country + "!" );
+        }
+    }
+
+    public class Lion : Animal
+    {
+        public Lion() : base() { this.WhatAnimal = "Lion"; }
+
+        public Lion( string _country, bool _hide, string _name ) : base( _country, _hide, _name ) { this.WhatAnimal = "Lion"; }
+
+        public override Enum GetClassificationAnimal()
+        {
+            return (eClassificationAnimal)2;
+        }
+
+        public override Enum GetFavouriteFood()
+        {
+            return (eFavouriteFood)1;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine( "Hello! I am " + WhatAnimal + " " + Name + " from " + Country + "!" );
+        }
+    }
+
+    public class Pig : Animal
+    {
+        public Pig() : base() { this.WhatAnimal = "Pig"; }
+
+        public Pig( string _country, bool _hide, string _name ) : base( _country, _hide, _name ) { this.WhatAnimal = "Pig"; }
+
+        public override Enum GetClassificationAnimal()
+        {
+            return (eClassificationAnimal)3;
+        }
+
+        public override Enum GetFavouriteFood()
+        {
+            return (eFavouriteFood)3;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine( "Hello! I am " + WhatAnimal + " " + Name + " from " + Country + "!" );
+        }
+    }
+
+    public class UserAttribute
+    {
+        public string Name { get; set; }
+
+        public string Comment { get; set; }
+
+        UserAttribute( string _name, string _comment ) { Name = _name; Comment = _comment; }
+
+        public static List<UserAttribute> MyList = new List<UserAttribute>();
+
+        static UserAttribute()
+        {
+            MyList.Add( new UserAttribute( "eClassificationAnimal", "Classification class for animals" ) );
+            MyList.Add( new UserAttribute( "eFavouriteFood", "Class for favourite animals food" ) );
+            MyList.Add( new UserAttribute( "Animal", "Base abstract class" ) );
+            MyList.Add( new UserAttribute( "Cow", "Cow, Derived class from Animal class" ) );
+            MyList.Add( new UserAttribute( "Lion", "Lion, Derived class from Animal class" ) );
+            MyList.Add( new UserAttribute( "Pig", "Pig, Derived class from Animal class" ) );
+            MyList.Add( new UserAttribute( "UserAttribute", "Specifies classes' atributes" ) );
+        }
+
+        public static string getComment( string name )
+        {
+            foreach (var pos in MyList) if (pos.Name == name) return pos.Comment;
+            return null;
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,135 @@
+﻿using System.Xml.Serialization;
+using System.IO;
+using System.IO.Compression;
+using Animals;
+using System.Text;
+
+class Program
+{
+
+
+    static string DirSearch( string startDirectory, string fileName )
+    {
+        //  Обработка исключения ошибки доступа
+        try
+        {
+            var testname = Directory.GetFiles(startDirectory);
+        }
+        catch (System.UnauthorizedAccessException)
+        {
+            return null;
+        }
+
+        var names = Directory.GetFiles(startDirectory);
+        foreach (var pos in names)
+        {
+            if (pos.Substring( pos.Length - fileName.Length ) == fileName)
+                return pos;
+        }
+
+        var dirs = Directory.GetDirectories(startDirectory);
+        foreach (var pos in dirs)
+        {
+            string str = DirSearch( pos, fileName );
+            if (str != null)
+                return str;
+        }
+        return null;
+    }
+
+    static void Main()
+    {
+
+        string fileName = "Lab08File.xml";
+        ///*
+
+        // Сериализация в файл
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Lion));
+            var fout = new FileStream(fileName, FileMode.OpenOrCreate);
+            var SerializedLion = new Lion("Africa", false, "Simba");
+            Console.WriteLine( "Class instance before serialization:" );
+            SerializedLion.Print();
+            Console.WriteLine();
+            xmlSerializer.Serialize( fout, SerializedLion );
+            fout.Close();
+        }
+        // Десериализация из файла
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Lion));
+            var fin = new FileStream(fileName, FileMode.OpenOrCreate);
+            Lion? DeserializedLion = xmlSerializer.Deserialize(fin) as Lion;
+            Console.WriteLine( "Deserialized instance of the class:" );
+            DeserializedLion.SayHello();
+            DeserializedLion.Print();
+            fin.Close();
+        }
+        //*/
+        /*
+        // Задание 2
+        Console.OutputEncoding = Encoding.UTF8;
+        //Console.Write( "File name: " );
+        //string? fileName = Console.ReadLine();
+        Console.Write( "Start directory: " );
+        string? startDirectory = Console.ReadLine();
+        Console.WriteLine();
+
+        string? filepath = DirSearch(startDirectory, fileName );
+        Console.WriteLine( filepath );
+        if (filepath == null)
+        {
+            Console.WriteLine( "The file was not found in the specified directory" );
+        }
+        else
+        {
+            using (var fsSource = new FileStream( filepath, FileMode.Open, FileAccess.Read ))
+            {
+                byte[] bytes = new byte[fsSource.Length];
+                int numBytesToRead = (int)fsSource.Length;
+                int numBytesReaded = 0;
+
+                while (numBytesToRead > 0)
+                {
+                    int n = fsSource.Read(bytes, numBytesReaded, numBytesToRead);
+                    if (n == 0) break;
+                    numBytesReaded += n;
+                    numBytesToRead -= n;
+                }
+
+                numBytesToRead = bytes.Length;
+
+                Console.WriteLine( "File Contents:" );
+                foreach (var pos in bytes)
+                {
+                    Console.Write( (char)pos );
+                }
+                Console.WriteLine();
+                Console.WriteLine( "End of file!" );
+                Console.WriteLine();
+
+                fsSource.Close();
+            }
+            // Ниже описан алгоритм сжатия файла стандартными методами .NET
+            // Имя будущего сжатого файла
+            string ZipFile = "./lab08file.zip";
+            // Создаём файл архива и подключаем к нему поток
+            FileStream zipStream = File.Create(ZipFile);
+            // Создаём архив, в который потом запишем необходимый нам файл
+            using ZipArchive archive = new(zipStream, ZipArchiveMode.Create);
+            // Точка входа для данных из файла в архив
+            ZipArchiveEntry entry = archive.CreateEntry(Path.GetFileName(filepath));
+            // Создаём поток чтения данных из несжатого файла
+            using FileStream inputStream = File.OpenRead(filepath);
+            // Создаём поток для записи данных в архив
+            using Stream outputStream = entry.Open();
+            // Записываем данные в архив
+            inputStream.CopyTo( outputStream );
+
+
+            Console.WriteLine( "Archiving is complete!" );
+            DirSearch( startDirectory, "lab08file.zip" );
+        }
+
+        */
+    }
+}
